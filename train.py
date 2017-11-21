7a59547a53c409f1208f98d901d2ea137ac68876
PAN001 <pan.atopos@gmail.com>
2017-11-21 17:19:43 -0500

    update

--- train.py
+++ train.py
@@ -81,6 +81,7 @@ opt = torch.optim.Adam(net.parameters(), lr = lr)
 criterion = CTCLoss()
 
 dataset_data = np.load("./dataset/data_5_10000.npy")
+dataset_data = dataset_data / 255.0
 dataset_labels = np.load("./dataset/labels_5_10000.npy")
 dataset_labels = dataset_labels.astype(int)
 
