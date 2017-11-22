74f005b88db3181d96189cc2a0bab6d8f017342b
PAN001 <pan.atopos@gmail.com>
2017-11-22 01:52:11 -0500

    update

--- CTCLoss_ref.py
+++ CTCLoss_ref.py
@@ -93,74 +93,74 @@ class CTCLoss(torch.autograd.Function):
         print("target label:   ", self.sequence)
         return torch.FloatTensor([loss])
 
-    # @staticmethod
-    def backward(self, grad_output, blank=10):
-        gradList = []
-        for i in range(self.batch_size):
-            params = self.parameters[i]
-            seq = self.sequence[i]
-            llForward = self.llForwardList[i]
-
-            seqLen = seq.shape[0]  # Length of label sequence (# phones)
-            numphones = params.shape[0]  # Number of labels
-            L = 2 * seqLen + 1  # Length of label sequence with blanks
-            T = params.shape[1]  # Length of utterance (time)
-            betas = np.zeros((L, T))
-            # Initialize betas and backwards pass
-            alphas = self.alphas[i]
-            betas[-1, -1] = params[blank, -1]
-            betas[-2, -1] = params[seq[-1], -1]
-            c = np.sum(betas[:, -1])
-            betas[:, -1] = betas[:, -1] / c
-            llBackward = np.log(c)
-            for t in range(T - 2, -1, -1):
-                start = max(0, L - 2 * (T - t))
-                end = min(2 * t + 2, L)
-                for s in range(end - 1, -1, -1):
-                    l = (s - 1) // 2
-                    # blank
-                    if s % 2 == 0:
-                        if s == L - 1:
-                            betas[s, t] = betas[s, t + 1] * params[blank, t]
-                        else:
-                            betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1]) * params[blank, t]
-                    # same label twice
-                    elif s == L - 2 or seq[l] == seq[l + 1]:
-                        betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1]) * params[seq[l], t]
-                    else:
-                        betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1] + betas[s + 2, t + 1]) * params[seq[l], t]
-
-                c = np.sum(betas[start:end, t])
-                betas[start:end, t] = betas[start:end, t] / c
-                llBackward += np.log(c)
-
-            # Compute gradient with respect to unnormalized input parameters
-            grad = np.zeros(params.shape)
-            ab = alphas * betas
-            for s in range(L):
-                # blank
-                if s % 2 == 0:
-                    grad[blank, :] += ab[s, :]
-                    ab[s, :] = ab[s, :] / params[blank, :]
-                else:
-                    grad[seq[(s - 1) // 2], :] += ab[s, :]
-                    ab[s, :] = ab[s, :] / (params[seq[(s - 1) // 2], :])
-            absum = np.sum(ab, axis=0)
-
-            # Check for underflow or zeros in denominator of gradient
-            llDiff = np.abs(llForward + llBackward)
-            # print("llDiff",llDiff)
-            # print("forward",llForward)
-            # print("backward",llBackward)
-            if llDiff > 1e-5 or np.sum(absum == 0) > 0:
-                print("Diff in forward/backward LL : %f", llDiff)
-                print("Zeros found : (%d/%d)", (np.sum(absum == 0), absum.shape[0]))
-                return torch.FloatTensor(gradList), None
-
-            grad = params - grad / (params * absum)
-            gradList.append(grad)
-        # print(gradList)
-        return torch.FloatTensor(gradList), None
+    # # @staticmethod
+    # def backward(self, grad_output, blank=10):
+    #     gradList = []
+    #     for i in range(self.batch_size):
+    #         params = self.parameters[i]
+    #         seq = self.sequence[i]
+    #         llForward = self.llForwardList[i]
+    #
+    #         seqLen = seq.shape[0]  # Length of label sequence (# phones)
+    #         numphones = params.shape[0]  # Number of labels
+    #         L = 2 * seqLen + 1  # Length of label sequence with blanks
+    #         T = params.shape[1]  # Length of utterance (time)
+    #         betas = np.zeros((L, T))
+    #         # Initialize betas and backwards pass
+    #         alphas = self.alphas[i]
+    #         betas[-1, -1] = params[blank, -1]
+    #         betas[-2, -1] = params[seq[-1], -1]
+    #         c = np.sum(betas[:, -1])
+    #         betas[:, -1] = betas[:, -1] / c
+    #         llBackward = np.log(c)
+    #         for t in range(T - 2, -1, -1):
+    #             start = max(0, L - 2 * (T - t))
+    #             end = min(2 * t + 2, L)
+    #             for s in range(end - 1, -1, -1):
+    #                 l = (s - 1) // 2
+    #                 # blank
+    #                 if s % 2 == 0:
+    #                     if s == L - 1:
+    #                         betas[s, t] = betas[s, t + 1] * params[blank, t]
+    #                     else:
+    #                         betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1]) * params[blank, t]
+    #                 # same label twice
+    #                 elif s == L - 2 or seq[l] == seq[l + 1]:
+    #                     betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1]) * params[seq[l], t]
+    #                 else:
+    #                     betas[s, t] = (betas[s, t + 1] + betas[s + 1, t + 1] + betas[s + 2, t + 1]) * params[seq[l], t]
+    #
+    #             c = np.sum(betas[start:end, t])
+    #             betas[start:end, t] = betas[start:end, t] / c
+    #             llBackward += np.log(c)
+    #
+    #         # Compute gradient with respect to unnormalized input parameters
+    #         grad = np.zeros(params.shape)
+    #         ab = alphas * betas
+    #         for s in range(L):
+    #             # blank
+    #             if s % 2 == 0:
+    #                 grad[blank, :] += ab[s, :]
+    #                 ab[s, :] = ab[s, :] / params[blank, :]
+    #             else:
+    #                 grad[seq[(s - 1) // 2], :] += ab[s, :]
+    #                 ab[s, :] = ab[s, :] / (params[seq[(s - 1) // 2], :])
+    #         absum = np.sum(ab, axis=0)
+    #
+    #         # Check for underflow or zeros in denominator of gradient
+    #         llDiff = np.abs(llForward + llBackward)
+    #         # print("llDiff",llDiff)
+    #         # print("forward",llForward)
+    #         # print("backward",llBackward)
+    #         if llDiff > 1e-5 or np.sum(absum == 0) > 0:
+    #             print("Diff in forward/backward LL : %f", llDiff)
+    #             print("Zeros found : (%d/%d)", (np.sum(absum == 0), absum.shape[0]))
+    #             return torch.FloatTensor(gradList), None
+    #
+    #         grad = params - grad / (params * absum)
+    #         gradList.append(grad)
+    #     # print(gradList)
+    #     return torch.FloatTensor(gradList), None
 
     def backward(self, grad_output):
         """
