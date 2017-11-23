8180d37fd7a6de6195e9f98ddc5847793b54dd18
PAN001 <pan.atopos@gmail.com>
2017-11-23 00:16:59 -0500

    update

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ class Net(nn.Module):
 
 
         # LSTM
-        self.lstm_input_size = self.image_H * self.cnn_output_chanel  # number of features = H * cnn_output_chanel = 32 * 32 = 1024
+        self.lstm_input_size = self.image_H * self.con2_output_chanel  # number of features = H * cnn_output_chanel = 32 * 32 = 1024
         self.lstm_hidden_size = 32
         self.lstm_num_layers = 1
         self.lstm_hidden = None
