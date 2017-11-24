83a926dceb620b9aebf8522ea64ef282d0179044
PAN001 <pan.atopos@gmail.com>
2017-11-24 15:50:11 -0500

    update

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ parser.add_argument('--cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=5, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--eval', action='store_true', default=False,
                     help='evaluate a pretrained model')
