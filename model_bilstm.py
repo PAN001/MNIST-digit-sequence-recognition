dce2c80b23aa7bd5743574fa99980542bfa82c90
PAN001 <pan.atopos@gmail.com>
2017-11-26 12:25:11 -0500

    update

--- model_bilstm.py
+++ model_bilstm.py
@@ -35,7 +35,7 @@ class Net(nn.Module):
         # conv2
         self.conv2_input_chanel = 10
         self.conv2_output_chanel = 20
-        self.conv2_kernelsize = (1, 2)
+        self.conv2_kernelsize = (5, 5)
         self.conv2 = nn.Conv2d(self.conv2_input_chanel, self.conv2_output_chanel, self.conv2_kernelsize)
 
         # initialization
