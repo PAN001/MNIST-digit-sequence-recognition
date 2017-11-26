f9acd7dbe593bc2c05f8edf9d033ef22f6a904d7
PAN001 <pan.atopos@gmail.com>
2017-11-26 12:31:41 -0500

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python main.py --cuda --epoch 1000 --lr 0.01 --model-path model_best_single_LSTM.pt --eval
+python main.py --cuda --epoch 1000 --lr 0.01 --model-path model_bilstm_20_10000_20epoch.pt --eval
