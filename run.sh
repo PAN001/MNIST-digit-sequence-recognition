8478c738dff4df8e4a5abc05a8be9c0ec3ed549e
PAN001 <pan.atopos@gmail.com>
2017-11-26 18:16:43 -0500

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 3lstm_1cnn_100 --batch-size 8
+python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 2lstm_2cnn_100 --batch-size 8
