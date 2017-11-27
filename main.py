218ac6262ff18fac437437bed07f177e5f1bfde7
PAN001 <pan.atopos@gmail.com>
2017-11-26 20:06:22 -0500

    update

--- main.py
+++ main.py
@@ -78,6 +78,12 @@ def train(epoch):
             print "label[0]: "
             print target.data.cpu().numpy()[0] if args.cuda else target.data.numpy()[0]
 
+            target_np = target.data.cpu().numpy() if args.cuda else target.data.numpy()
+            test_target_np = target_np[0].reshpe(1, -1)
+
+            edit_dists, _, _, _, _ = decoder.edit_distance(test_target_np, predictions)
+            print "Edit distance is: ", edit_dists[0]
+
             print ""
 
         # log
