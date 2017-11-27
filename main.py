789c8b1f6bff552acac7802debded7574d19f181
PAN001 <pan.atopos@gmail.com>
2017-11-26 23:36:56 -0500

    update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 import torch.utils.data as data_utils
 import numpy as np
-# from model_1lstm_2cnn import *
-from model_bilstm import *
+from model_1lstm_2cnn import *
+# from model_bilstm import *
 # from model_org import *
 # from model_2bilstm import *
 from CTCLoss import *
--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 # python main.py --cuda --epoch 1000 --lr 0.005 --log-interval 1 --id org --batch-size 32 --model-path org_best_model.pt
 
 # model-2
+python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 1lstm_2cnn_100 --batch-size 16 --model-path 1lstm_2cnn_100_best_model.pt
 
 # model-3
-python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 1bilstm_2cnn_100 --batch-size 16 --model-path 1bilstm_2cnn_100_best_model.pt
+# python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 1bilstm_2cnn_100 --batch-size 16 --model-path 1bilstm_2cnn_100_best_model.pt
 
 # model-4
 # python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 2bilstm_2cnn_100 --batch-size 16 --model-path 2bilstm_2cnn_100_best_model.pt
