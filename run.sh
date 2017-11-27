0e259d618967d584f4cd78c887dc5a2034d43c90
PAN001 <pan.atopos@gmail.com>
2017-11-26 23:05:09 -0500

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 # python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 2bilstm_2cnn_100 --batch-size 16
 
-python main.py --cuda --epoch 1000 --lr 0.005 --log-interval 1 --id org --batch-size 32 --model-path org_best_model.pt
+# model-1
+# python main.py --cuda --epoch 1000 --lr 0.005 --log-interval 1 --id org --batch-size 32 --model-path org_best_model.pt
+
+# model-4
+python main.py --cuda --epoch 1000 --lr 0.01 --log-interval 1 --id 2bilstm_2cnn_100 --batch-size 16 --model-path 2bilstm_2cnn_100_best_model.pt
