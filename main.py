448247e6b3943ac78e6a3e92daadcb30c7297704
PAN001 <pan.atopos@gmail.com>
2017-11-30 12:31:24 -0500

    update

--- main.py
+++ main.py
@@ -252,8 +252,8 @@ print "validation_log_path: ", validation_log_path
 train_data_path = "./dataset/data_" + args.train_len + "_10000_random.npy"
 train_labels_path = "./dataset/labels_" + args.train_len + "_10000_random.npy"
 
-validate_data_path = "./dataset/data_" + args.test_len + "_random_1000.npy"
-validate_labels_path = "./dataset/labels_" + args.test_len + "_random_1000.npy"
+validate_data_path = "./dataset/data_" + args.test_len + "_1000_random.npy"
+validate_labels_path = "./dataset/labels_" + args.test_len + "_1000_random.npy"
 
 # validate_data_path = "./dataset/test_data_" + args.test_len + "_sun.npy"
 # validate_labels_path = "./dataset/test_labels_" + args.test_len + "_sun.npy"
