a7456a295d7faf2581c019f2175fd16452bdcaa1
PAN001 <pan.atopos@gmail.com>
2017-12-01 16:00:08 -0500

    update test script

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-python main.py --cuda --model-path 2bilstm_2cnn_100_best_model.pt --eval --validate-batch-size 1000
+# Generate dataset
+python mkSeqMNIST.py --N 100 --M 1000 
+
+# Load pre-trained model and test
+python main.py --cuda --model-path 2scnn_2bilstm_scaled_100_best_model.pt --eval --test-len 100
